269e250d22fc33eb260ec6fd56f4a40280352269
Yauhen Shulitski <jsnjack@gmail.com>
2020-09-04 16:58:38 +0200

    Update files

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById("myModal");
+const btn = document.getElementById('tnc');
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+  
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
